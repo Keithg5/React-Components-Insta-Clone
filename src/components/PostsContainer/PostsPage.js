64064f5cebb5c2017eb9f2a99f2a5f6634c32fe9
Keith Giddings Jr <Keithgiddings@hotmail.com>
2020-03-27 04:26:13 -0400

    finished MVP

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
+import React, { useState } from "react";
 // import useState
 import React from "react";
+
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from '../../dummy-data'
 
 const PostsPage = () => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
+      {PostsPage.map(p => )
+        <Post key={p.imageURL} dataFromParent={p} />))}
       {/* map through data here to return a Post and pass data as props to Post */}
+      
     </div>
   );
 };
